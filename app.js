332d956507fb6266430ebb3da007687828505abd
Mahtab Alam <alammahtab08@gmail.com>
2017-07-15 21:15:21 +0530

    Done

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ var bodyParser=require('body-parser');
 var neo4j = require('neo4j-driver').v1;
 
 var app=express();
+app.use(express.static('public'));
 
 var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','awesome'));
 var session = driver.session();
 
 
 
-app.get('/', function(req,res) {
+/*app.get('/', function(req,res) {
     session.run('MATCH (n:Restaurant) RETURN n')
            .then(function(result){
-           	 //console.log("All result "+ JSON.stringify(result))
-           	  /*result.records.forEach(function(record){
+           	 
+           	  result.records.forEach(function(record){
            	  	console.log(JSON.stringify(record._fields[0]))
            	  	res.status(200).send(JSON.stringify(record._fields[0]))
-           	  })*/
+           	  })
            	  res.status(200).send(result.records);
            })
            .catch(function(err){
            	console.log("Error "+err);
            })
 });
+*/
+
+app.get('/',function(req,res){
+	res.sendFile('index.html',{ root: __dirname });
+})
 
 
 var port = process.env.PORT || 8080;
---
+++ public/app.js
@@ -0,0 +1,35 @@
+var app=angular.module('swiggyHack',['ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('index', {
+            url: '/',
+            templateUrl: 'templates/main.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller:'LoginController'
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'templates/signup.html',
+            controller:'SignupController'
+        })
+        .state('list', {
+            url: '/list',
+            templateUrl: 'templates/list.html',
+            controller:'ListController',
+            authenticate: true
+        })
+        .state('edit', {
+            url: '/edit/:id',
+            templateUrl: 'templates/edit-bookmark.html',
+            controller:'EditController',
+            authenticate: true
+        });
+});
+
